1f042de092d7f80ce66395a60c4cac944f8e1dc8
angelvinyals <angel.vinyals@gmail.com>
2018-03-10 12:27:36 +0100

    refactor TodoForm passing props

--- src/components/todo/TodoForm.js
+++ src/components/todo/TodoForm.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-export const TodoForm = (props) => (
+export const TodoForm = ({currentTodo='', handleInputChange}) => (
   <div>
     <form action="" id='idForm'>
       <input 
         type="text" 
-        value={props.currentTodo} 
-        onChange={props.handleInputChange}
+        value={currentTodo} 
+        onChange={handleInputChange}
       />
     </form>
   </div>
